98509133b140aa0501ac449d7f9f268bf7649bd8
Jona2808 <jonathan28quina@gmail.com>
2023-04-21 18:54:52 -0500

    Agregar archivos de proyecto.

---
+++ refuerzoS3/Login.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace refuerzoS3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInicial_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Principal());
+        }
+
+        private void btnRegistrar_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Registro());
+        }
+    }
+}
